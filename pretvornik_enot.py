c2e18e75c8854e98d79a8ce48afaa347eb79dcc0
Doroteja Mazej <teja.mazej@gmail.com>
2017-02-01 08:56:28 +0100

    Add files via upload

---
+++ pretvornik_enot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+st_milj = 5
+pretvornik_km = 0.6214
+
+st_km = int(raw_input("Dobrodošli v pretvorniku enot iz km v milje. Vnesite željeno št. km"))
+
+st_milj = st_km * pretvornik_km
+print st_milj
